c04c80c3f4d00f8c82749eb2845a3b333e904097
carlos-loaeza <carlos.loaeza@ibm.com>
2023-04-10 17:20:06 -0600

    Eureka Server - Servidor de registro de nombre

--- customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -115,9 +115,9 @@ public class CustomerRestController {
 
     private String getProductName(Long id) {
         WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
-                .baseUrl("http://localhost:8083/product")
+                .baseUrl("http://service-products/product")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+                .defaultUriVariables(Collections.singletonMap("url", "http://service-products/product"))
                 .build();
         JsonNode block = build.method(HttpMethod.GET).uri("/" + id)
                 .retrieve().bodyToMono(JsonNode.class).block();
@@ -127,7 +127,7 @@ public class CustomerRestController {
 
     private List<?> getTransactions(String iban) {
         WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
-                .baseUrl("http://localhost:8082/transaction")
+                .baseUrl("http://service-transaction/transaction")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
